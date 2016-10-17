d00473b5815f963541e6c8cdb16d87bc27353534
xiaoping jin <jinxp@lenovo.com>
2016-10-17 17:12:16 +0800

    add indicator ajax

---
+++ web/server.js
@@ -0,0 +1,21 @@
+var http=require("http");
+
+var server=http.createServer(function(req,res){
+
+    if(req.url!=="/favicon.ico"){
+
+        //res.writeHead(200,{"Content-Type":"text/plain","Access-Control-Allow-Origin":"http://localhost:8080"});
+		//res.write("hello,我是从服务器端接收的");
+		res.writeHead(200, {"Content-Type":"application/json","Access-Control-Allow-Origin":"http://localhost:8080"});
+		res.write(JSON.stringify({data:'hello,我是从服务器端接收的'}));
+    }
+
+    res.end();
+
+});
+
+server.listen(8888,"localhost",function(){
+
+    console.log("开始监听...");
+
+});
