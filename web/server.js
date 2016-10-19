2b3a2d2d72a3f92aa683d7b30b17f23bc2636b8c
xiaoping jin <jinxp@lenovo.com>
2016-10-19 11:03:42 +0800

    fix bug

--- web/server.js
+++ web/server.js
@@ -1,13 +1,24 @@
 var http=require("http");
+var url = require("url");
 
 var server=http.createServer(function(req,res){
-	console.log(req.body);
-    if(req.url!=="/favicon.ico"){
 
+    if(req.url!=="/favicon.ico"){
+	    var m = url.parse(req.url, true);
+	    console.log(m.query["mode"])
         //res.writeHead(200,{"Content-Type":"text/plain","Access-Control-Allow-Origin":"http://localhost:8080"});
 		//res.write("hello,我是从服务器端接收的");
 		res.writeHead(200, {"Content-Type":"application/json","Access-Control-Allow-Origin":"http://localhost:8080"});
-		res.write(JSON.stringify({data:'hello,我是从服务器端接收的'}));
+		if(m.query["mode"]!= null) {
+			if(m.query["mode"] == 0) {
+				res.write(JSON.stringify({data:1}));
+			} else {
+				res.write(JSON.stringify({data:0}));
+			}
+		} else {
+			res.write(JSON.stringify({data:"hello,我是从服务器端接收的"}));
+		}
+		
     }
 
     res.end();
